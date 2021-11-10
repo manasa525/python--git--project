c19e09f5e9982307ddeed9c6273e129f926f0526
manasa <manasasrinivas525@gmail.com>
2021-11-10 15:19:11 +0530

    maddy apps

---
+++ main.py
@@ -0,0 +1,19 @@
+def isPerfectSquare(x):
+ 
+    
+    if(x >= 0):
+        sr = math.sqrt(x)
+        
+    
+        return ((sr*sr) == float(x))
+    return false
+ 
+
+ 
+ 
+x = 2502
+if (isPerfectSquare(x)):
+    print("Yes")
+else:
+    print("No")
+ 
---
+++ main1.py
@@ -0,0 +1,17 @@
+def isPalindrome(s):
+    
+    rev = ''.join(reversed(s))
+ 
+    # Checking if both string are
+    # equal or not
+    if (s == rev):
+        return True
+    return False
+ 
+s = "malayalam"
+ans = isPalindrome(s)
+ 
+if (ans):
+    print("Yes")
+else:
+    print("No")
